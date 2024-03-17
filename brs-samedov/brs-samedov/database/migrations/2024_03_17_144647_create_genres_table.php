0c93f03699a1afb3f462d2e1db1498d4317b2c04
Aleksandr <alekcandr206@gmail.com>
2024-03-17 17:12:24 +0100

    start assignment

--- brs-samedov/brs-samedov/database/migrations/2024_03_17_144647_create_genres_table.php
+++ brs-samedov/brs-samedov/database/migrations/2024_03_17_144647_create_genres_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('genres', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolval('is_librarian')->default('false');
-            $table->rememberToken();
+            $table->string('name', 255);
+            $table->enum('style', ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark']);
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('genres');
     }
 };
